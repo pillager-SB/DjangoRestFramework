012caae27126f50a74679f13069b9b30d7ea4337
pillager-SB <78688006+pillager-SB@users.noreply.github.com>
2022-08-08 16:35:04 +0300

    create const users in Apps.js file

--- todo/frontend/src/App.js
+++ todo/frontend/src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import UserList from "./components/User";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'users': []
+        }
+    }
+
+    componentDidMount() {
+        const users = [
+            {
+                "username": "user1",
+                "first_name": 'Фёдор',
+                "last_name": 'Достоевский',
+                "email": "user1@usr.com",
+            },
+            {
+                "username": "user2",
+                "first_name": 'Нобунага',
+                "last_name": 'Ода',
+                "email": "user2@usr.com",
+
+            },
+            {
+                "username": "user3",
+                "first_name": "Лев",
+                "last_name": "Троцкий",
+                "email": "user3@usr.com",
+
+            },
+            {
+                "username": "admin",
+                "first_name": "Джабба",
+                "last_name": "Хатт",
+                "email": "admin@usr.com",
+
+            }
+        ]
+        this.setState(
+            {
+                'users':users
+            }
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                <UserList users={this.state.users}/>
+            </div>
+        )
+    }
 }
 
 export default App;
