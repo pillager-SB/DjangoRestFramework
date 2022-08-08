dadae3b1dd9e95c189171bf958595f25eec9c61f
pillager-SB <78688006+pillager-SB@users.noreply.github.com>
2022-08-08 17:12:08 +0300

    get response from API

--- todo/frontend/src/App.js
+++ todo/frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React from "react";
 import UserList from "./components/User";
+import axios from "axios";
 
 class App extends React.Component {
     constructor(props) {
@@ -12,40 +13,43 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        const users = [
+        axios.get('http://127.0.0.1:8000/api/users/').then(response =>{
+            this.setState(
             {
-                "username": "user1",
-                "first_name": 'Фёдор',
-                "last_name": 'Достоевский',
-                "email": "user1@usr.com",
-            },
-            {
-                "username": "user2",
-                "first_name": 'Нобунага',
-                "last_name": 'Ода',
-                "email": "user2@usr.com",
-
-            },
-            {
-                "username": "user3",
-                "first_name": "Лев",
-                "last_name": "Троцкий",
-                "email": "user3@usr.com",
-
-            },
-            {
-                "username": "admin",
-                "first_name": "Джабба",
-                "last_name": "Хатт",
-                "email": "admin@usr.com",
-
-            }
-        ]
-        this.setState(
-            {
-                'users':users
+                'users':response.data
             }
         )
+        }).catch(error => console.log(error))
+        // const users = [
+        //     {
+        //         "username": "user1",
+        //         "first_name": 'Фёдор',
+        //         "last_name": 'Достоевский',
+        //         "email": "user1@usr.com",
+        //     },
+        //     {
+        //         "username": "user2",
+        //         "first_name": 'Нобунага',
+        //         "last_name": 'Ода',
+        //         "email": "user2@usr.com",
+        //
+        //     },
+        //     {
+        //         "username": "user3",
+        //         "first_name": "Лев",
+        //         "last_name": "Троцкий",
+        //         "email": "user3@usr.com",
+        //
+        //     },
+        //     {
+        //         "username": "admin",
+        //         "first_name": "Джабба",
+        //         "last_name": "Хатт",
+        //         "email": "admin@usr.com",
+        //
+        //     }
+        // ]
+
     }
 
     render() {
--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -27,7 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -39,13 +42,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-
     'users',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
