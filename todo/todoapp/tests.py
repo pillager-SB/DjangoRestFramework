a3292bd423c9ccc518c8e97467a93b0ea2414a69
pillager-SB <78688006+pillager-SB@users.noreply.github.com>
2022-08-31 12:25:20 +0300

    APIClient tests

--- todo/todoapp/tests.py
+++ todo/todoapp/tests.py
@@ -40,5 +40,30 @@ class TestTodoUserViewSet(TestCase):
         response = view(request)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
+    def test_client_get(self):
+        user = TodoUser.objects.create(username='user3', first_name='Lala', last_name='Sasa', email='user3@user.com')
+        client = APIClient()
+        response = client.get(f'/api/users/{user.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_client_guest(self):
+        user = TodoUser.objects.create(username='user3', first_name='Lala', last_name='Sasa', email='user3@user.com')
+        client = APIClient()
+        response = client.put(f'/api/users/{user.id}/', {'first_name': 'RaRa'})
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+    def test_client_create_admin(self):
+        user = TodoUser.objects.create(username='user3', first_name='Lala', last_name='Sasa', email='user3@user.com')
+        client = APIClient()
+        admin = TodoUser.objects.create_superuser('adm1', 'adm1@usr.com', 'adm_0123')
+        client.login(username='adm', password='adm_0123')
+        response = client.put(f'/api/users/{user.id}/', {'username': 'user4', 'first_name': 'RaRa', 'last_name': 'Zaza',
+                                                         'email': 'user4@user.com'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        user = TodoUser.objects.get(id=user.id)
+        self.assertEqual(user.first_name, 'RaRa')
+        client.logout()
+
     def tearDown(self):
         pass
