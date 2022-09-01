4741477981e6a9bfda672d00f7a5e29dd4c72ec1
pillager-SB <78688006+pillager-SB@users.noreply.github.com>
2022-09-01 18:47:33 +0300

    QueryParameterVersioning is On

--- todo/users/serializers.py
+++ todo/users/serializers.py
@@ -6,3 +6,9 @@ class TodoUserSerializer(ModelSerializer):
     class Meta:
         model = TodoUser
         fields = ('id', 'username', 'first_name', 'last_name', 'email')
+
+
+class TodoUserSerializerBase(ModelSerializer):
+    class Meta:
+        model = TodoUser
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'is_superuser', 'is_staff')
